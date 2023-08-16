76ffccff139caa6c220e158b0eccd7afcb2ce548
OEM Configuration (temporary user) <fr_bensalem@esi.dz>
2023-08-16 01:17:40 +0100

    load to clickhouse

--- airflow/dags/etl.py
+++ airflow/dags/etl.py
@@ -112,8 +112,47 @@ with DAG(
         dag=dag
     )
 
+    def df_to_rows(df):
+        return df.values.tolist()
+
     def load():
-        return 3
+        client = clickhouse_connect.get_client(host='192.168.0.56',port="18123")
+        products=pd.read_csv('/opt/airflow/datasets/products-generated.csv')
+        customers=pd.read_csv('/opt/airflow/datasets/customers-transformed.csv')
+        baskets=pd.read_csv('/opt/airflow/datasets/basket_details.csv')
+
+        products=products[['base_price', 'discount_percentage', 'quantity', 'manufacturer',
+       'tax_amount', 'product_id', 'category', 'sku', 'taxless_price',
+       'unit_discount_amount', 'min_price', 'discount_amount',
+       'created_on', 'product_name', 'price', 'taxful_price',
+       'base_unit_price']]
+        products['created_on']=pd.to_datetime(products['created_on'])
+        products['manufacturer']=products['manufacturer'].astype('str')
+        products['sku']=products['sku'].astype('str')
+        products['product_name']=products['product_name'].astype('str')
+        products['category']=products['category'].astype('str')
+        client.command("""CREATE TABLE IF NOT EXISTS products (base_price Decimal32(2), discount_percentage Decimal32(2), quantity UInt16, manufacturer LowCardinality(Nullable(String)),
+       tax_amount Decimal32(2), product_id UInt32, category Enum('Men\\'s Clothing', 'Women\\'s Clothing','Women\\'s Shoes','Men\\'s Accessories', 'Men\\'s Shoes','Women\\'s Accessories'), sku String, taxless_price Decimal32(2),
+       unit_discount_amount Decimal32(3), min_price Decimal32(2), discount_amount Decimal32(3),
+       created_on DateTime64(3), product_name Nullable(String), price Decimal32(2), taxful_price Decimal32(2),
+       base_unit_price Decimal32(2)) ENGINE MergeTree ORDER BY product_id""")
+
+        client.insert('products', df_to_rows(products.loc[0:products.shape[0]]), column_names=['base_price', 'discount_percentage', 'quantity', 'manufacturer',
+       'tax_amount', 'product_id', 'category', 'sku', 'taxless_price',
+       'unit_discount_amount', 'min_price', 'discount_amount',
+       'created_on', 'product_name', 'price', 'taxful_price',
+       'base_unit_price'])
+
+        baskets['basket_date']=pd.to_datetime(baskets['basket_date'])
+        client.command("CREATE TABLE IF NOT EXISTS baskets (customer_id UInt32, product_id UInt32, basket_date Date, basket_count UInt8) ENGINE MergeTree ORDER BY (customer_id, product_id)")
+        client.insert('baskets', df_to_rows(baskets.loc[0:baskets.shape[0]]), column_names=['customer_id', 'product_id', 'basket_date', 'basket_count'])
+
+        customers['sex']=customers['sex'].astype('str')
+        client.command("CREATE TABLE IF NOT EXISTS customers (customer_id UInt32, sex Enum('male' = 1, 'female' = 2,'UNKNOWN'= 3), customer_age UInt64, tenure UInt8) ENGINE MergeTree ORDER BY customer_id")
+        client.insert('customers', df_to_rows(customers.loc[0:customers.shape[0]]), column_names=['customer_id', 'sex', 'customer_age','tenure'])
+        
+        result=client.command("show tables")
+        assert result=="baskets\ncustomers\nproducts"
 
     load_task=PythonOperator(
         task_id='load',
